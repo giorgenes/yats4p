81a3afb8c72f985694195b7172fa4b731bc2bc78
Giorgenes Gelatti <giorgenes@gmail.com>
2012-03-29 00:40:22 +0100

    root => tasks#index

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ Yats4p::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'tasks#index'
 
   # See how all your routes lay out with "rake routes"
 
