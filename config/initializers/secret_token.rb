2bfd479ef8558315655dfaf1adeb92e534df1f0e
Giorgenes Gelatti <giorgenes@gmail.com>
2012-03-28 23:45:04 +0100

    initial rails import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yats4p::Application.config.secret_token = '96479b881424b502f8e1f2ec02c4b2333233f855e2468958013c7301f87c9ef6832ba9d92fe0a7b3bf6cb0e81d7fc58a684aa1af9893230c7f2da3ee8c14beb1'
