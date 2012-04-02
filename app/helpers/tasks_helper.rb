6a20a9fc04a8a7651a2b86c47a8b093a6c4ecdf4
Giorgenes Gelatti <giorgenes@gmail.com>
2012-04-02 17:23:07 +0100

    fixed fork me on github

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,8 @@
 module TasksHelper
+  def done_undone_button(task)
+    link_to (task.completed ? "Not done!" : "Done!"),
+      task_path(task, {:task => {:completed => !task.completed}}),
+      :method => :put,
+      :class => "btn btn-mini btn-#{task.completed ? 'action' : 'success'}" 
+  end
 end
