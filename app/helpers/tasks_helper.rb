d5bf104a43bfaab8ee217ec06de9cc8fd23d991d
Giorgenes Gelatti <giorgenes@gmail.com>
2012-04-02 17:42:11 +0100

    improved task list view

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -3,6 +3,6 @@ module TasksHelper
     link_to (task.completed ? "Not done!" : "Done!"),
       task_path(task, {:task => {:completed => !task.completed}}),
       :method => :put,
-      :class => "btn btn-mini btn-#{task.completed ? 'action' : 'success'}" 
+      :class => "btn btn-mini pull-right btn-#{task.completed ? 'action' : 'success'}" 
   end
 end
